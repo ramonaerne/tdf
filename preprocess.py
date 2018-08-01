ef9e2154e35f159f0b01d395bc008bd8ebd04e9e
ramonaerne <raemaerne@gmail.com>
2018-08-01 11:11:35 +0200

    complete preprocess script and hdf storage

--- preprocess.py
+++ preprocess.py
@@ -7,6 +7,15 @@ from PIL import Image
 from pytesseract import image_to_string
 from itertools import groupby
 
+# overlay windows:
+# <----------------------> width_name
+# <-----------------------------------> width_overlay
+# | G. Thomas ----------- | FLAG | SKY | TIME
+#                                 <---> width_team = 80
+#                                     <-> white_pad
+# topleft corner is at (x1_overlay, y1_overlay)
+# time is overlain in white otherwise could be different label
+
 # parameters
 x1_overlay = 169
 y1_overlay = 890
@@ -23,26 +32,23 @@ maxdev = 5
 windowsize = 5
 refcolor = np.array([246, 170, 41], dtype=np.uint8)
 whiterefcolor = np.array([255, 250, 252], dtype=np.uint8)
-
-# overlay windows:
-# <----------------------> width_name
-# <-----------------------------------> width_overlay
-# | G. Thomas ----------- | FLAG | SKY | TIME
-#                                 <---> width_team = 80
-#                                     <-> white_pad
-# topleft corner is at (x1_overlay, y1_overlay)
-# time is overlain in white otherwise could be different label
-
 fps = 1
 
+DEBUG_FLAG = 0
+
+print('loading video and prepare for analysis')
 # load frame
 clip = VideoFileClip("tdf2018-20.mp4").without_audio().set_fps(fps)
 
 # save frame for reference pixel finding
-clip.save_frame("frame_riding.png", t='03:06:30')
-clip.save_frame("frame_finish.png", t='03:07:00')
+if DEBUG_FLAG:
+    clip.save_frame("frame_riding.png", t='03:06:30')
+    clip.save_frame("frame_finish.png", t='03:07:00')
 
-subclip = clip.subclip('00:00:00', '00:05:00')
+substart = '00:00:00'
+subend   = '00:30:00'
+#%%
+subclip = clip.subclip(substart, subend)
 # crop subframes
 overlay_clip = crop(subclip,
                     x1=x1_overlay,
@@ -72,6 +78,7 @@ whitecolor = crop(subclip,
 #whitecolor.write_videofile("whitecolor.mp4")
 
 #%%
+print('performing color analysis')
 # loops over each color channel and returns true if all channels are within range
 def color_in_range(pixels, refcolor):
     color = np.round(np.average(np.reshape(pixels, [-1, 3]), axis=0))
@@ -89,6 +96,7 @@ isRiderLabelFrame = [c & w for (c,w) in zip(colormatches, whitematches)]
 
 #%% zip same array with lag of one to detect changes, then write the new value
 # as well as the index into the new list, also prepend the first element
+print('calculating color changes')
 def colorchange(lst):
     out = [(0, lst[0])]
     lst_short = lst[:-1]
@@ -107,22 +115,24 @@ riderLabelTransition = colorchange(isRiderLabelFrame)
 # possibly two same values can occur after filtering
 # also sum of duration might not match actual duration since the
 # duration of the spikes is removed
+print('removing spikes in data')
 def filterspikes(lst):
     out = []
     lst_short = lst[:-1]
     lst_lag_plus_one = lst[1:]
     for ((i1, v1), (i2, v2)) in zip(lst_short, lst_lag_plus_one):
-        duration = (i2-i1) / clip.fps
+        duration = (i2-i1) / subclip.fps
         if duration >= 1:
             out += [(i1, v1, i2-i1)]
 
     # now add last frame (that runs until the stream ends)
     last_idx, last_val = lst[-1]
-    out += [(last_idx, last_val, floor(clip.end*clip.fps)-last_idx)]
+    out += [(last_idx, last_val, floor(subclip.end*subclip.fps)-last_idx)]
     return out
 
 filteredTransitions = filterspikes(riderLabelTransition)
-print(filteredTransitions)
+if DEBUG_FLAG:
+    print(filteredTransitions)
 
 #%% merge duplicates in a row
 
@@ -130,6 +140,7 @@ print(filteredTransitions)
 # of removing them calculate new duration as
 # duration_unique = start_duplicate + duration_duplicate - start_unique
 # where duplicate is only the last of the duplicates in a row
+print('merging identical subsequent values')
 def mergefiltered(filteredchanges):
     (_, prevkey, _) = filteredchanges[0]
     out = []
@@ -157,8 +168,6 @@ def mergefiltered(filteredchanges):
     return out + lastel
 
 mergedTransitions = mergefiltered(filteredTransitions)
-print("merged")
-print(mergedTransitions)
 
 #%% get desired output
 # - only take true values
@@ -168,19 +177,38 @@ print(mergedTransitions)
 
 riderLabel = [(i,i+d,i/fps, (i+d)/fps) for (i,v,d) in mergedTransitions
               if v == True]
+
+print("riders:")
+print(riderLabel)
 #%% ocr
+print('performing ocr')
 name = []
 team = []
-def performOCR(clip, frameid):
-    imarr = clip.get_frame(frameid)
+def performOCR(subclip, frameid):
+    imarr = subclip.get_frame(frameid)
     im = Image.fromarray(imarr)
     return image_to_string(im)
 
 # perform ocr for center shot of every element
-for (istart,iend,tstart,tend) in riderlabel:
+for (istart,iend,tstart,tend) in riderLabel:
     centerframeid = round((istart + iend)/2)
     name += [performOCR(name_clip, centerframeid)]
     team += [performOCR(team_clip, centerframeid)]
 
 print(name)
 print(team)
+
+
+
+#%% now zip everything
+import pandas as pd
+print('putting it together and store to hdf')
+riderCoverageRaw = [(tstart, tend, name, team) for ((_,_,tstart, tend), name, team) in zip(riderLabel, name, team)]
+print(riderCoverageRaw)
+df = pd.DataFrame(riderCoverageRaw)
+
+# and save
+store = pd.HDFStore('tdf_coverage_raw.h5')
+store[substart + '-' + subend] = df
+
+store.close()
